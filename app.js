3778c02c38c23ea0f8a72cf5830902148663ad48
khngjx <khngjx@gmail.com>
2020-11-12 09:44:07 +0000

    testing for push message trigger

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.listen(8080, () => {
- console.log("Server run on port 3000");
+ console.log("Server running on the port 3000");
 });
 
 app.get("/", (req, res) => {
